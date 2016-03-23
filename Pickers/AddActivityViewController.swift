ef07002f98f2839a5760441a10a94c78338e07e3
Kaitlyn Barbara Montour <kmontour@ccl-gmlw177-x06.uits.uwm.edu>
2016-03-23 16:39:00 -0500

    added classes and view controllers

---
+++ Pickers/AddActivityViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddActivityViewController.swift
+//  Pickers
+//
+//  Created by Kaitlyn Barbara Montour on 3/23/16.
+//  Copyright (c) 2016 Kaitlyn Barbara Montour. All rights reserved.
+//
+
+import UIKit
+
+class AddActivityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
